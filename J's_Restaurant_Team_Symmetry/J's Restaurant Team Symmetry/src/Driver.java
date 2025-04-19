32cd7f7e784588068b521befb3d7af9921d9712d
Matthew Pfleger <mattpfleger20@gmail.com>
2025-04-19 16:05:06 -0400

    Delete J's_Restaurant_Team_Symmetry/J's Restaurant Team Symmetry directory

--- J's_Restaurant_Team_Symmetry/J's Restaurant Team Symmetry/src/Driver.java
+++
@@ -1,6 +0,0 @@
-public class Driver {
-    public static void main(String[] args) {
-        //Home.showHomeScreen();
-        ManagerPage.showManagerPage(); // Show the manager page directly for testing
-    }
-}
